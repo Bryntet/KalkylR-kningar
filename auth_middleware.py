1631dc6015e0161b97a9bca594dd9e49780b65ae
Edvin <edvinbryntesson@gmail.com>
2022-08-13 15:00:00 +0200

    Authentication stuff

---
+++ auth_middleware.py
@@ -0,0 +1,34 @@
+from functools import wraps
+import jwt
+from flask import request, abort
+from flask import current_app
+import models
+
+
+def token_required(f):
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+        if not token:
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unauthorized"
+            }, 401
+        try:
+            if token == current_app.config["SECRET_KEY"]:
+                return f(*args, **kwargs)
+                        
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+
+        return f(*args, **kwargs)
+
+    return decorated
