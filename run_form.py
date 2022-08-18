5d3ce60ae4aea7dde64ad3f86f64ea3b72bd7588
Edvin <edvinbryntesson@gmail.com>
2022-08-18 16:50:14 +0200

    form testing

---
+++ run_form.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/form')
+def test():
+   return render_template('form.html')
+if __name__ == '__main__':
+   app.run(host='0.0.0.0')
